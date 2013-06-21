985f2fb640c6d4659c77c1cba00eace54a83563e
Olivier Louvignes <olivier@mg-crea.com>
2013-06-21 14:59:24 +0200

    fix(directive): prevent incorrect reloads

--- src/pull-to-refresh.js
+++ src/pull-to-refresh.js
@@ -10,7 +10,17 @@
 
 angular.module('mgcrea.pullToRefresh', [])
 
-  .directive('pullToRefresh', function($compile, $timeout, $q) {
+  .constant('pullToRefreshConfig', {
+    treshold: 40,
+    debounce: 400,
+    messages: {
+      pull: 'pull to refresh',
+      release: 'release to refresh',
+      loading: 'refreshing...'
+    }
+  })
+
+  .directive('pullToRefresh', function($compile, $timeout, $q, pullToRefreshConfig) {
 
     // https://github.com/EightMedia/hammer.js/blob/master/examples/pull-to-refresh.html
 
@@ -19,15 +29,6 @@ angular.module('mgcrea.pullToRefresh', [])
       '<i class="icon-refresh" ng-class="{\'icon-arrow-down\':status===\'pull\', \'icon-arrow-up\':status===\'release\', \'icon-refresh icon-spin\':status===\'loading\'}"></i>' +
       '&nbsp;<span class="ptr-message">{{messages[status]}}</span>' +
     '</div>';
-
-    var config = {
-      debounce: 400,
-      messages: {
-        pull: 'pull to refresh',
-        release: 'release to refresh',
-        loading: 'refreshing...'
-      }
-    };
 
     return {
       restrict: 'A',
@@ -39,32 +40,32 @@ angular.module('mgcrea.pullToRefresh', [])
 
         return function postLink(scope, iElement, iAttrs, controller) {
 
+          var config = angular.extend({}, pullToRefreshConfig, iAttrs);
+
+          // Initialize isolated scope vars
           scope.$loading = false;
           scope.messages = config.messages;
           scope.status = 'pull';
 
-          var height = 20;
-          var pullMargin = -2 * height + 'px auto 0';
-
+          // Compile customized pElement against the directive's scope
           $timeout(function(){
             $compile(pElement)(scope);
           });
 
-          iElement.bind('touchstart', function(ev) {
-            var top = iElement[0].scrollTop;
-            // if(!top) iElement[0].scrollTop = 1;
-          });
+          // iElement.bind('touchstart', function(ev) {
+          //   var top = iElement[0].scrollTop;
+          // });
 
-          var reload = false;
+          var shouldReload = false;
           iElement.bind('touchmove', function(ev) {
             var top = iElement[0].scrollTop;
-            if(top < -40 && !reload) {
-              reload = true;
+            if(top < -config.treshold && !shouldReload) {
+              shouldReload = true;
               scope.$apply(function() {
                 scope.status = 'release';
               });
-            } else if(top > -40 && reload) {
-              reload = false;
+            } else if(top > -config.treshold && shouldReload) {
+              shouldReload = false;
               scope.$apply(function() {
                 scope.status = 'pull';
               });
@@ -72,19 +73,23 @@ angular.module('mgcrea.pullToRefresh', [])
           });
 
           iElement.bind('touchend', function(ev) {
-            var top = iElement[0].scrollTop;
-            if(!reload) return;
+            if(!shouldReload) return;
+            shouldReload = false;
             scope.$apply(function() {
+
               scope.$loading = true;
               scope.status = 'loading';
+
               var start = +new Date();
               $q.when(scope.$eval(iAttrs.pullToRefresh))
               .then(function() {
                 var elapsed = +new Date() - start;
+
                 $timeout(function() {
                   scope.$loading = false;
                   scope.status = 'pull';
                 }, elapsed < config.debounce ? config.debounce - elapsed : 0);
+
               });
             });
           });
