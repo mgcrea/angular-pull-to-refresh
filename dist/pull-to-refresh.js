5984ad35b2fa0600d8cb17c5781f58845fe5c51f
Olivier Louvignes <olivier@mg-crea.com>
2013-06-21 15:00:09 +0200

    feat(release): bump v0.2.2

--- dist/pull-to-refresh.js
+++ dist/pull-to-refresh.js
@@ -1,18 +1,19 @@
 'use strict';
-angular.module('mgcrea.pullToRefresh', []).directive('pullToRefresh', [
+angular.module('mgcrea.pullToRefresh', []).constant('pullToRefreshConfig', {
+  treshold: 40,
+  debounce: 400,
+  messages: {
+    pull: 'pull to refresh',
+    release: 'release to refresh',
+    loading: 'refreshing...'
+  }
+}).directive('pullToRefresh', [
   '$compile',
   '$timeout',
   '$q',
-  function ($compile, $timeout, $q) {
+  'pullToRefreshConfig',
+  function ($compile, $timeout, $q, pullToRefreshConfig) {
     var template = '' + '<div class="ptr" ng-class="\'ptr-\'+status">' + '<i class="icon-refresh" ng-class="{\'icon-arrow-down\':status===\'pull\', \'icon-arrow-up\':status===\'release\', \'icon-refresh icon-spin\':status===\'loading\'}"></i>' + '&nbsp;<span class="ptr-message">{{messages[status]}}</span>' + '</div>';
-    var config = {
-        debounce: 400,
-        messages: {
-          pull: 'pull to refresh',
-          release: 'release to refresh',
-          loading: 'refreshing...'
-        }
-      };
     return {
       restrict: 'A',
       scope: true,
@@ -20,36 +21,32 @@ angular.module('mgcrea.pullToRefresh', []).directive('pullToRefresh', [
         tElement.prepend(template);
         var pElement = tElement.find('.pull-to-refresh');
         return function postLink(scope, iElement, iAttrs, controller) {
+          var config = angular.extend({}, pullToRefreshConfig, iAttrs);
           scope.$loading = false;
           scope.messages = config.messages;
           scope.status = 'pull';
-          var height = 20;
-          var pullMargin = -2 * height + 'px auto 0';
           $timeout(function () {
             $compile(pElement)(scope);
           });
-          iElement.bind('touchstart', function (ev) {
-            var top = iElement[0].scrollTop;
-          });
-          var reload = false;
+          var shouldReload = false;
           iElement.bind('touchmove', function (ev) {
             var top = iElement[0].scrollTop;
-            if (top < -40 && !reload) {
-              reload = true;
+            if (top < -config.treshold && !shouldReload) {
+              shouldReload = true;
               scope.$apply(function () {
                 scope.status = 'release';
               });
-            } else if (top > -40 && reload) {
-              reload = false;
+            } else if (top > -config.treshold && shouldReload) {
+              shouldReload = false;
               scope.$apply(function () {
                 scope.status = 'pull';
               });
             }
           });
           iElement.bind('touchend', function (ev) {
-            var top = iElement[0].scrollTop;
-            if (!reload)
+            if (!shouldReload)
               return;
+            shouldReload = false;
             scope.$apply(function () {
               scope.$loading = true;
               scope.status = 'loading';
