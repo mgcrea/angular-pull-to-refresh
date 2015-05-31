2b312d34990ceeb3deca340c42dc77c987578204
Johnny <johnny.iday@gmail.com>
2015-05-31 14:16:37 +0800

    Update angular-pull-to-refresh.js
    add non s support

--- src/angular-pull-to-refresh.js
+++ src/angular-pull-to-refresh.js
@@ -44,9 +44,110 @@ angular.module('mgcrea.pullToRefresh', [])
             });
           };
 
+          var notIOS = function() {
+  			  	var pan = {
+  					  enabled: false,
+  					  distance: 0,
+  					  startingPositionY: 0
+  			  	};
+  			  
+  			  /**
+  				 * Determine whether pan events should apply based on scroll position on panstart
+  				 * 
+  				 * @param {object} e - Event object
+  				 */
+  				var _panStart = function(e) {
+  					pan.startingPositionY = document.body.scrollTop;
+  					if ( pan.startingPositionY === 0 ) {
+  						pan.enabled = true;
+  					}
+  				};
+
+  				/**
+  				 * Handle element on screen movement when the pandown events is firing.
+  				 * 
+  				 * @param {object} e - Event object
+  				 */
+  				var _panDown = function(e) {
+  					if ( ! pan.enabled ) {
+  						return;
+  					}
+
+  					e.preventDefault();
+  					pan.distance = e.distance / 2.5;
+
+  					_setContentPan();
+//  					_setBodyClass();
+  				};
+
+  				/**
+  				 * Handle element on screen movement when the pandown events is firing.
+  				 * 
+  				 * @param {object} e - Event object
+  				 */
+  				var _panUp = function(e) {
+  					if ( ! pan.enabled || pan.distance === 0 ) {
+  						return;
+  					}
+
+  					e.preventDefault();
+
+  					if ( pan.distance < e.distance / 2.5 ) {
+  						pan.distance = 0;
+  					} else {
+  						pan.distance = e.distance / 2.5;
+  					}
+
+  					_setContentPan();
+//  					_setBodyClass();
+  				};
+
+  				/**
+  				 * Set the CSS transform on the content element to move it on the screen.
+  				 */
+  				var _setContentPan = function() {
+  					// Use transforms to smoothly animate elements on desktop and mobile devices
+  					iElement[0].style.transform = iElement[0].style.webkitTransform = 'translateY(' + pan.distance + 'px)';
+  				};
+
+  				/**
+  				 * Determine how to animate and position elements when the panend event fires.
+  				 * 
+  				 * @param {object} e - Event object
+  				 */
+  				var _panEnd = function(e) {
+  					if ( ! pan.enabled ) {
+  						return;
+  					}
+
+  					e.preventDefault();
+  					iElement[0].style.transform = iElement[0].style.webkitTransform = '';
+
+  					pan.distance = 0;
+  					pan.enabled = false;
+  				};
+
+            	var h = new Hammer( iElement[0] );
+  			  	h.get( 'pan' ).set( { direction: Hammer.DIRECTION_VERTICAL } );
+  			  	h.on( 'panstart', _panStart );
+  			  	h.on( 'pandown', _panDown );
+  			  	h.on( 'panup', _panUp );
+  			  	h.on( 'panend', _panEnd );
+            };
+            if (!navigator.userAgent.match(/iOS/i)) {
+            	notIOS();
+            };
+            
+
           var shouldReload = false;
           iElement.bind('touchmove', function(ev) {
             var top = scrollElement[0].scrollTop;
+            if (!navigator.userAgent.match(/iOS/i)) {
+            	var transform = iElement[0].style.transform;
+            	if (transform) {
+            	  top = -transform.replace(/translateY\(['"]?([\d.]+)px['"]?\)/i, "$1");
+            	}
+            }
             if(top < -config.treshold && !shouldReload) {
               setStatus('release');
             } else if(top > -config.treshold && shouldReload) {
